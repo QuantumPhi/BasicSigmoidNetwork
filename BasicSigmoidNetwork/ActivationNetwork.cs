8949bababd1c0676357ae9544a752ebfd32ce247
Tarik Onalan <phi.quantum@gmail.com>
2014-10-09 15:58:44 -0700

    Sigmoid activation function

---
+++ BasicSigmoidNetwork/ActivationNetwork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSigmoidNetwork
+{
+    class ActivationNetwork
+    {
+        public readonly int InCount { get; private set; }
+        public readonly int HideCount { get; private set; }
+
+        public double[] WeightsInput { get; private set; }
+        public double[] WeightsHidden { get; private set; }
+
+        public double Run(double[] input, double output)
+        {
+            Trace.Assert(input.Length == WeightsInput.Length);
+            double sum = Enumerable
+                .Range(0, input.Length)
+                .Select(x =>
+                {
+
+                })
+                .Sum();
+            double sumHidden;
+            return sumHidden > 0 ? 1 : 0;
+        }
+    }
+}
---
+++ BasicSigmoidNetwork/IActivationFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSigmoidNetwork
+{
+    interface IActivationFunction
+    {
+        public double Function(double x);
+
+        public double Derivative(double x);
+    }
+
+    public class Sigmoid : IActivationFunction
+    {
+        public double Function(double x)
+        {
+            return (1D) / (1 + Math.Exp(x));
+        }
+
+        public double Derivative(double x)
+        {
+            var e = Math.Exp(-x);
+            return -(e) / (1 + 2D * e + Math.Pow(e, 2D));
+        }
+    }
+}
