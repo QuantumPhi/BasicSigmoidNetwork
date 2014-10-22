c56ff6413e4f23108ebff27331519e0f6b42043b
Tarik Onalan <phi.quantum@gmail.com>
2014-10-21 22:22:48 -0700

    Probably got it wrong

--- BasicSigmoidNetwork/ActivationNetwork.cs
+++ BasicSigmoidNetwork/ActivationNetwork.cs
@@ -9,24 +9,68 @@ namespace BasicSigmoidNetwork
 {
     class ActivationNetwork
     {
-        public readonly int InCount { get; private set; }
-        public readonly int HideCount { get; private set; }
+        public readonly double Alpha { get; private set; }
+        public readonly int Inputs { get; private set; }
+        public readonly int Hidden { get; private set; }
 
         public double[] WeightsInput { get; private set; }
         public double[] WeightsHidden { get; private set; }
 
-        public double Run(double[] input, double output)
+        public readonly IActivationFunction activate { get; private set; }
+
+        public ActivationNetwork(IActivationFunction function, double alpha, int input, int hidden)
         {
-            Trace.Assert(input.Length == WeightsInput.Length);
-            double sum = Enumerable
-                .Range(0, input.Length)
+            this.activate = function;
+            Alpha = alpha;
+            WeightsInput = new double[(Inputs = input)];
+            WeightsHidden = new double[(Hidden = hidden)];
+        }
+
+        public double Compute(double[] input)
+        {
+            Trace.Assert(input.Length == Inputs);
+
+            double sum = activate.Function(Enumerable
+                .Range(0, Inputs)
+                .Select(x => WeightsInput[x] * input[x])
+                .Sum());
+
+            double sumHidden = activate.Derivative(Enumerable
+                .Range(0, Hidden)
+                .Select(x => WeightsHidden[x] * sum)
+                .Sum());
+
+            return sumHidden > 0 ? 1 : 0;
+        }
+
+        public double Train(double[] input, double output)
+        {
+            double error = 0.5 * Enumerable
+                .Range(0, Inputs)
+                .Select(x => Math.Pow(output - Compute(input), 2))
+                .Sum();
+
+            WeightsHidden = Enumerable
+                .Range(0, Hidden)
+                .Select(x => Alpha * (output - Compute(input)) * activate.Derivative(error) * input[x])
+                .ToArray();
+
+            WeightsInput = Enumerable
+                .Range(0, Inputs)
                 .Select(x =>
                 {
+                    double errorHidden = Enumerable
+                        .Range(0, Hidden)
+                        .Select(y => (output - Compute(input)) * activate.Derivative(error) * WeightsHidden[y])
+                        .Sum();
 
+                    double errorInput = errorHidden * activate.Function(errorHidden) * WeightsInput[x];
+
+                    return errorInput;
                 })
-                .Sum();
-            double sumHidden;
-            return sumHidden > 0 ? 1 : 0;
+                .ToArray();
+
+            return (1 - error) / 100;
         }
     }
 }
